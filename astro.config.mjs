9c0030eca37733201e7007e6ba2445a8cc8baa2f
wanto-production <ikwansatria3974@gmail.com>
2025-03-25 18:51:49 +0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,18 @@ import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://portofolio-ikhwan.vercel.app",
   output: "static",
   integrations: [react(), svelte(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
+
