c5ca37adfe5d9ff7fffd14f81d4945dc06cab73f
wanto-production <ikwansatria3974@gmail.com>
2025-03-27 17:20:44 +0700

    update commit

--- src/components/Planetmodel/Canvas.svelte
+++ src/components/Planetmodel/Canvas.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-    import { Canvas } from "@threlte/core";
-    import Model from "./Model.svelte";
+  import { Canvas } from "@threlte/core";
 </script>
 
 <div
-    class=" w-[400px] h-[450px] lg:w-[500px] lg:h-[500px] rounded-md overflow-hidden"
+  class=" w-[400px] h-[450px] lg:w-[500px] lg:h-[500px] rounded-md overflow-hidden"
 >
-    <Canvas>
-        <Model />
-    </Canvas>
+  <Canvas>
+    {#await import("./Model.svelte")}
+      <div
+        class=" w-[400px] h-[400px] bg-light-grey rounded-md grid place-content-center"
+      >
+        <h2 class="text-black">wait 3D model loading...</h2>
+      </div>
+    {:then { default: Model }}
+      <Model />
+    {/await}
+  </Canvas>
 </div>
--- src/components/Planetmodel/Model.svelte
+++ src/components/Planetmodel/Model.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
   import { T, useTask } from "@threlte/core";
-  //@ts-ignore
   import { useGltf } from "@threlte/extras";
   import { speed, size } from "./state";
-  import { Skeleton } from "../ui/skeleton.tsx";
 
-  let { children } = $props();
   let rotateY = $state(0);
 
   useTask((delta) => {
@@ -13,12 +10,6 @@
   });
 </script>
 
-{#await useGltf(import.meta.env.PUBLIC_GLB_URL)}
-  <div
-    class=" w-[400px] h-[400px] bg-light-grey rounded-md grid place-content-center"
-  >
-    <h2 class="text-black">wait 3D model loading...</h2>
-  </div>
-{:then model}
+{#await useGltf(import.meta.env.PUBLIC_GLB_URL) then model}
   <T is={model.scene} scale={$size} rotation.x={0.4} rotation.y={rotateY} />
 {/await}
