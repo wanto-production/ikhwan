a4690256ad41742c6fbfc6b91c4cd77149088406
wanto-production <ikwansatria3974@gmail.com>
2025-03-27 17:15:44 +0700

    update commit

--- src/components/Planetmodel/Model.svelte
+++ src/components/Planetmodel/Model.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import { T, useTask } from "@threlte/core";
+  //@ts-ignore
   import { useGltf } from "@threlte/extras";
   import { speed, size } from "./state";
+  import { Skeleton } from "../ui/skeleton.tsx";
 
+  let { children } = $props();
   let rotateY = $state(0);
 
   useTask((delta) => {
@@ -10,6 +13,12 @@
   });
 </script>
 
-{#await useGltf(import.meta.env.GLB_URL) then model}
+{#await useGltf(import.meta.env.PUBLIC_GLB_URL)}
+  <div
+    class=" w-[400px] h-[400px] bg-light-grey rounded-md grid place-content-center"
+  >
+    <h2 class="text-black">wait 3D model loading...</h2>
+  </div>
+{:then model}
   <T is={model.scene} scale={$size} rotation.x={0.4} rotation.y={rotateY} />
 {/await}
