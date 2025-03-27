0b1294168ec75293ff9e348b3f73be0645ca01f5
wanto-production <ikwansatria3974@gmail.com>
2025-03-27 16:48:07 +0700

    update commit

--- src/components/Planetmodel/Model.svelte
+++ src/components/Planetmodel/Model.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-    import { T, useTask } from "@threlte/core";
-    import { useGltf } from "@threlte/extras";
-    import { speed, size } from "./state";
+  import { T, useTask } from "@threlte/core";
+  import { useGltf } from "@threlte/extras";
+  import { speed, size } from "./state";
 
-    let rotateY = $state(0);
+  let rotateY = $state(0);
 
-    useTask((delta) => {
-        rotateY += delta * $speed;
-    });
+  useTask((delta) => {
+    rotateY += delta * $speed;
+  });
 </script>
 
-{#await useGltf("/planet.glb") then model}
-    <T is={model.scene} scale={$size} rotation.x={0.4} rotation.y={rotateY} />
+{#await useGltf(import.meta.env.GLB_URL) then model}
+  <T is={model.scene} scale={$size} rotation.x={0.4} rotation.y={rotateY} />
 {/await}
